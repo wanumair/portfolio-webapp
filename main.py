6ddefecf472be9470522951414e8ab9f3032f398
wanumair <wanumair1209@gmail.com>
2024-07-09 01:23:17 +0800

    add description to new column

--- main.py
+++ main.py
@@ -4,13 +4,22 @@ st.set_page_config(layout="wide")
 
 col1, col2 = st.columns(2)
 
+
+
+
 with col1:
     st.image("images/umair.JPG")
 
 with col2:
     st.title("Wan Umair")
-    content = """
-wan umair
+    content1 = """
+    My name is Wan Umair, I am a self learner and coder and I hope I gain some knowledge and experience doing this.     
     """
-    st.info(content)
+    st.info(content1)
+
+content2 = """
+Below u can find my projects. I mainly create these projects from tutorials that I watched from various "
+         "sources, but I do try to self practice and do better implementation and functionality.
+"""
+st.write(content2)
 
